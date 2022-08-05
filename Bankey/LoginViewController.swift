9b233c3b40c118fe142f3fc6f3443b7ebdef7c2a
spartannik <dead5042@gmail.com>
2022-08-05 15:35:01 +0300

    Login Button Done

--- Bankey/LoginViewController.swift
+++ Bankey/LoginViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let loginView = LoginView()
-
+    let signInButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        style()
@@ -21,15 +22,39 @@ class LoginViewController: UIViewController {
 extension LoginViewController {
     private func style() {
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8
+        signInButton.setTitle("Sign In", for: [])
+        signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        
+        
     }
     
     private func layout() {
         view.addSubview(loginView)
+        view.addSubview(signInButton)
         
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
         ])
+        
+        // Button
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+        
+        
+    }
+}
+
+extension LoginViewController {
+    @IBAction func signInTapped(sender: UIButton) {
+        
     }
 }
